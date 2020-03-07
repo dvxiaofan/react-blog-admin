2c9c4f71890ffd730b589f95d067bae3282f4f02
xiaofan <1353815633@qq.com>
2020-03-07 16:38:12 +0800

    组件化搭建后台管理页面UI

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import AdminIndex from './AdminIndex';
 import Login from './Login';
 
 function Main() {
 	return (
 		<Router>
 			<Route path='/login/' exact component={Login} />
+			<Route path='/index/' exact component={AdminIndex}  />
 		</Router>
 	);
 }
